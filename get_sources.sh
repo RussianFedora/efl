b39db59bdeaa5ca98460ca027d9dc59d8b39bc7c
Levitan <vlevitan91@gmail.com>
2012-12-20 17:04:44 +0400

    Add get source script

---
+++ get_sources.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ORIGNAME=efl
+VERSION=1.7.99
+SVN_REVISION=81177
+NAME=${ORIGNAME}-${VERSION}.svn${SVN_REVISION}
+
+rm -rf ${ORIGNAME}
+svn co -r $SVN_REVISION http://svn.enlightenment.org/svn/e/trunk/efl/ >/dev/null
+find ${ORIGNAME} -name ".svn" -exec rm -rf {} \; 2>/dev/null
+mv ${ORIGNAME} ${NAME}
+
+tar cfJ ${NAME}.tar.xz ${NAME}
+rm -rf ${NAME}
